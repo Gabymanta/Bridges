79b1dc4e50b0d6ca0528c3e50f7b466abe165a87
Gabymanta <gabymanta@gmail.com>
2026-02-07 20:32:05 -0500

    Add

--- Global/SignalHub.gd
+++ Global/SignalHub.gd
@@ -1,6 +1,8 @@
 extends Node
 
 
+
+signal on_level_complete(is_complete: bool)
 signal on_player_hit(lives: int, shake: bool)
 
 signal on_create_bullet(
@@ -8,6 +10,9 @@ signal on_create_bullet(
 	obj_type: Constants.ObjectType)
 
 signal on_create_object(pos: Vector2, obj_type: Constants.ObjectType)
+	
+	
+	
 	
 func emit_on_player_hit(lives: int, shake: bool):
 	on_player_hit.emit(lives, shake);
@@ -18,3 +23,6 @@ func emit_on_create_bullet(pos: Vector2, dir: Vector2,
 
 func emit_on_create_object(pos: Vector2, obj_type: Constants.ObjectType):
 	on_create_object.emit(pos, obj_type);
+	
+func emit_on_level_complete(is_complete: bool):
+	on_level_complete.emit(is_complete);
